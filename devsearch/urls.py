8088eb121bea51432bfb2c0dcd7312bf531f07c7
shalu john <shalu@yougotagift.com>
2023-08-11 12:28:27 +0530

    initial commit on devsearch/

---
+++ devsearch/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+# def projects(request):
+#     return HttpResponse('here are our products')
+#
+# def project(request,pk):
+#      return HttpResponse('single project' + ' ' + str(pk))
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('projects.urls')),
+    # path('projects/',projects, name="projects"),
+    # path('project/<str:pk>/',project, name="project"),
+    path(' ', include('users.urls')),
+]
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
