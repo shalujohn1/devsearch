eab8cf3bc0ea0e899a9da277737992727860f1b9
shalu john <shalu@yougotagift.com>
2023-09-01 14:23:30 +0530

    updated in sub3 static/js/main.js

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+
+<script type="text/javascript">
+  let searchForm = document.getElementById('searchForm')
+  let pageLinks = document.getElementsByClassName('page-link')
+
+  if(searchForm) {
+    for(let i=0; pageLinks.length > i; i++) {
+    pageLinks[i].addEventListener('click',function (e) {
+      e.preventDefault()
+
+      let page = this.dataset.page
+
+      searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+      searchForm.submit()
+
+    })
+   }
+  }
+</script>
