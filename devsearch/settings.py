6b7081b5c9db53e708dd2244585b3481204c3284
shalu john <shalu@yougotagift.com>
2023-09-04 18:12:41 +0530

    sub4 updated devsearch/settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -117,7 +117,16 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shalujohn2018@gmail.com'
+EMAIL_HOST_PASSWORD = 'rnkwrvotmzjwspry'
 
 
 # Static files (CSS, JavaScript, Images)
