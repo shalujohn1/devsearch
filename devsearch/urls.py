2e30b2263176161ceb26869e3f20ab75134c98e0
shalu john <shalu@yougotagift.com>
2023-09-04 18:13:00 +0530

    sub4 updated devsearch/urls.py

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -3,20 +3,28 @@ from django.urls import path,include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
-# def projects(request):
-#     return HttpResponse('here are our products')
-#
-# def project(request,pk):
-#      return HttpResponse('single project' + ' ' + str(pk))
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('projects.urls')),
-    # path('projects/',projects, name="projects"),
-    # path('project/<str:pk>/',project, name="project"),
+
     path('users/', include('users.urls')),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name="reset_password.html"),name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name="reset_password_sent.html"),name="password_reset_done"),
+
+path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(
+    template_name="reset.html"),name="password_reset_confirm"),
+
+path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(
+    template_name="reset_password_complete.html"),name="password_reset_complete"),
 ]
 
 
